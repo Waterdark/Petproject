21dca24076c9fe35eddadca04a49611b6b97361d
Waterdark <shadow_priest@mail.ru>
2020-05-21 18:42:17 +0500

    registered catalog app and change time zone

--- Petproject/settings.py
+++ Petproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig', #add catalog module(dir)
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Yekaterinburg' #time zone Chelybinsk
 
 USE_I18N = True
 
