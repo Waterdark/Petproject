e29c9112b7772a9347f4b6c739931f04b34192fa
Waterdark <shadow_priest@mail.ru>
2020-06-01 17:10:56 +0500

    add new variable nam_genre

--- catalog/views.py
+++ catalog/views.py
@@ -13,11 +13,13 @@ def index(request):
     # Доступные книги (статус = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # Метод 'all()' применен по умолчанию.
+    num_genre = Genre.objects.count()
     
     # Отрисовка HTML-шаблона index.html с данными внутри 
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,
+        'num_authors':num_authors,'num_genre':num_genre},
     )
