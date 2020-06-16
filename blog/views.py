176ee2ff92f6e5d33d641f48452247ee4578e4e3
Waterdark <shadow_priest@mail.ru>
2020-06-16 14:54:37 +0500

    sam hz chto tut

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def posts_list(request):
-    n = ['Pasha', 'Sasha','Masha','Anna','Vova']
+    posts = Post.objects.all()
     hello = 'Hello Chel!'
     number = 42
-    return render(request, 'blog/index.html', context={'names': n, 'hello': hello, 'number': number})
+    return render(request, 'blog/index.html', context={'posts':posts,'number':number})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact = slug)
+    return render(request, 'blog/post_detail.html', context={'post':post})
